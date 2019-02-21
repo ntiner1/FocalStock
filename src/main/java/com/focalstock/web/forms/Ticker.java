690fcbc9124b8c0130547892cf3a69b752a55ab0
nick <ntiner1@tamu.edu>
2019-02-21 10:37:56 -0600

    Added ticker and greeting practice forms

---
+++ src/main/java/com/focalstock/web/forms/Ticker.java
@@ -0,0 +1,10 @@
+package com.focalstock.web.forms;
+
+import lombok.Data;
+
+@Data
+public class Ticker {
+  
+  private String ticker;
+
+}
--- src/main/java/com/focalstock/web/home/HomeController.java
+++ src/main/java/com/focalstock/web/home/HomeController.java
@@ -1,15 +1,25 @@
 package com.focalstock.web.home;
 
+import com.focalstock.web.forms.Ticker;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
 
   @GetMapping("/")
   public String home(Model model) {
+    model.addAttribute("ticker", new Ticker());
     return "home";
+  }
+
+  @PostMapping("/")
+  public String tickerSubmit(@ModelAttribute Ticker ticker) {
+    return "info";
   }
 
 }
